ff59d9168e0baac277645a834d3d8efa9281386d
Roman Kaminskiy - pc <derrki@gmail.com>
2019-05-02 14:08:25 +0300

    Створено клас Purchase.java по аналогії з пеопереднім

---
+++ src/main/java/com/bimdog/testtask/model/Purchase.java
@@ -0,0 +1,49 @@
+package com.bimdog.testtask.model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class Purchase implements Serializable{
+
+    private Long id;
+    private Souvenir souvenir;
+    private GregorianCalendar date;
+    private String currency;
+
+    public Purchase (){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Souvenir getSouvenir() {
+        return souvenir;
+    }
+
+    public void setSouvenir(Souvenir souvenir) {
+        this.souvenir = souvenir;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Purchase{" +
+                "id=" + id +
+                ", souvenir=" + souvenir +
+                ", date=" + date +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
+}
