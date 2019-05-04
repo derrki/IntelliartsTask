2a60bcc27925b27a264ae5723077ae43356f6818
Roman Kaminskiy - pc <derrki@gmail.com>
2019-05-04 13:55:32 +0300

    Додано тестовий код вибірки даних з таблиці по даті

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,15 +19,21 @@ public class Main {
         String sql = "INSERT INTO purchases(date, name_souvenir, price, currency) VALUES(?, ?, ? , ?);";
         connection = DriverManager.getConnection(HOST_MYSQL, USERNAME_MYSQL, PASSWORD_MYSQL);
 
-        try (PreparedStatement pStatementTest = connection.prepareStatement(sql)){
+        try (Statement statement = connection.createStatement();
+                PreparedStatement pStatementTest = connection.prepareStatement(sql)){
 
             System.out.println("Приєднано до mysql");
 
-            pStatementTest.setDate(1, new Date(1556965285493L));
+            pStatementTest.setDate(1, new Date(1116005285493L));
             pStatementTest.setString(2, "T_shirt");
             pStatementTest.setInt(3, 20);
             pStatementTest.setString(4, "USD");
             pStatementTest.execute();
+
+            ResultSet resultSet = statement.executeQuery("select * from purchases WHERE date='2019-05-04'");
+            while (resultSet.next()){
+                System.out.println(resultSet.getDate("date"));
+            }
 
         } catch (SQLException e) {
             System.err.println("Приєднання до бази не відбулось, по причині - " + e);
