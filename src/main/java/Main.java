2e3ce178e48f991d6e5fa89ee05f940e458a8375
Roman Kaminskiy - pc <derrki@gmail.com>
2019-05-04 15:26:08 +0300

    Написано метод перетворення String в мілісекунди. При записі в базу даних відбувається зміщення на один день. !!!!!!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 import java.sql.*;
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * 1. Make it work. 2. Make it right. 3. Make it fast
  */
 public class Main {
-    public static void main(String[] args) throws SQLException {
+
+    public static final long oneDay = 86400000;
+
+    public static void main(String[] args) throws SQLException, ParseException {
         String HOST_MYSQL = "jdbc:mysql://localhost:3306/souvenir_shop"+
                 "?verifyServerCertificate=false"+
                 "&useSSL=false"+
@@ -24,15 +31,19 @@ public class Main {
 
             System.out.println("Приєднано до mysql");
 
-            pStatementTest.setDate(1, new Date(1116005285493L));
-            pStatementTest.setString(2, "T_shirt");
-            pStatementTest.setInt(3, 20);
-            pStatementTest.setString(4, "USD");
+            pStatementTest.setDate(1, new Date(parseDate("2000-09-01")));
+            pStatementTest.setString(2, "book");
+            pStatementTest.setInt(3, 50);
+            pStatementTest.setString(4, "UAH");
             pStatementTest.execute();
 
-            ResultSet resultSet = statement.executeQuery("select * from purchases WHERE date='2019-05-04'");
+            ResultSet resultSet = statement.executeQuery("select * from purchases WHERE date='1985-01-01'");
             while (resultSet.next()){
-                System.out.println(resultSet.getDate("date"));
+                System.out.print(resultSet.getDate("date") + " ");
+                System.out.print(resultSet.getString("name_souvenir") + " ");
+                System.out.print(resultSet.getInt("price") + " ");
+                System.out.print(resultSet.getString("currency"));
+                System.out.println();
             }
 
         } catch (SQLException e) {
@@ -42,5 +53,15 @@ public class Main {
                 connection.close();
             }
         }
+
+    }
+
+    public static long parseDate(String stringFormat) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        java.util.Date date = simpleDateFormat.parse(stringFormat);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        return calendar.getTimeInMillis();
     }
 }
