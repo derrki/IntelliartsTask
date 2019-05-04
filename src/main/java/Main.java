95c4b8974d99a55ef6613d2df4f175d3c9087df2
Roman Kaminskiy - pc <derrki@gmail.com>
2019-05-04 18:20:55 +0300

    Конект до бази даних винесено в окремий клас. Параметри конекту записано в файл config.properties

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import com.bimdog.testtask.dao.ConnectionFactory;
+
 import java.sql.*;
 import java.sql.Date;
 import java.text.ParseException;
@@ -12,28 +14,20 @@ public class Main {
     public static final long oneDay = 86400000;
 
     public static void main(String[] args) throws SQLException, ParseException {
-        String HOST_MYSQL = "jdbc:mysql://localhost:3306/souvenir_shop"+
-                "?verifyServerCertificate=false"+
-                "&useSSL=false"+
-                "&requireSSL=false"+
-                "&useLegacyDatetimeCode=false"+
-                "&amp"+
-                "&serverTimezone=UTC";
-        String USERNAME_MYSQL = "root";
-        String PASSWORD_MYSQL = "root";
-        Connection connection = null;
+
+        ConnectionFactory conFactory = ConnectionFactory.getInstance();
 
         String sql = "INSERT INTO purchases(date, name_souvenir, price, currency) VALUES(?, ?, ? , ?);";
-        connection = DriverManager.getConnection(HOST_MYSQL, USERNAME_MYSQL, PASSWORD_MYSQL);
 
-        try (Statement statement = connection.createStatement();
+        try (Connection connection = conFactory.getConnection();
+                Statement statement = connection.createStatement();
                 PreparedStatement pStatementTest = connection.prepareStatement(sql)){
 
             System.out.println("Приєднано до mysql");
 
-            pStatementTest.setDate(1, new Date(parseDate("2000-09-01")));
-            pStatementTest.setString(2, "book");
-            pStatementTest.setInt(3, 50);
+            pStatementTest.setDate(1, new Date(parseDate("2007-10-10")));
+            pStatementTest.setString(2, "pen");
+            pStatementTest.setInt(3, 100);
             pStatementTest.setString(4, "UAH");
             pStatementTest.execute();
 
@@ -45,23 +39,18 @@ public class Main {
                 System.out.print(resultSet.getString("currency"));
                 System.out.println();
             }
-
         } catch (SQLException e) {
             System.err.println("Приєднання до бази не відбулось, по причині - " + e);
-        }finally {
-            if (connection != null) {
-                connection.close();
-            }
         }
 
     }
 
     public static long parseDate(String stringFormat) throws ParseException {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-M-dd");
         java.util.Date date = simpleDateFormat.parse(stringFormat);
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
 
-        return calendar.getTimeInMillis();
+        return calendar.getTimeInMillis() + oneDay;
     }
 }
---
+++ src/main/java/com/bimdog/testtask/dao/ConnectionFactory.java
@@ -0,0 +1,54 @@
+package com.bimdog.testtask.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class ConnectionFactory {
+
+    private static ConnectionFactory instance = null;
+
+    private static String db_url;
+    private static String login;
+    private static String password;
+    private static String driver;
+
+    private ConnectionFactory(){
+        getProperties();
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(db_url, login, password);
+        } catch (SQLException e) {
+            System.out.println("ERROR: Unable to Connect to Database.");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static ConnectionFactory getInstance(){
+        if (instance == null){
+            instance = new ConnectionFactory();
+        }
+        return instance;
+    }
+
+
+    //одержуємо параметри конекту до БД з файлу config.properties
+    private void getProperties(){
+        ResourceBundle resource = ResourceBundle.getBundle("config");
+        db_url = resource.getString("DB_URL");
+        login = resource.getString("LOGIN");
+        password = resource.getString("PASSWORD");
+        driver = resource.getString("DRIVER");
+    }
+
+}
